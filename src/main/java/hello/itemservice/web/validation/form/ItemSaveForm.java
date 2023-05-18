fa7b90bf422aa27cededd25b4fb1d15984d0f3a5
pjione <a41924250@gmail.com>
2023-05-18 20:34:59 +0900

    폼 전송 객체 분리

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -0,0 +1,24 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ItemSaveForm {
+    @NotBlank
+    private String itemName;
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+    @NotNull
+    @Max(value = 9999)
+    private Integer quantity;
+}
