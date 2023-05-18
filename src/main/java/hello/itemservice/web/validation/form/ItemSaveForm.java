37442a156f7f5f37317856840d80e99a09be611b
pjione <a41924250@gmail.com>
2023-05-18 22:52:26 +0900

    Validation - api

--- src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
+++ src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.validation.form;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ItemSaveForm {
     @NotBlank
     private String itemName;
